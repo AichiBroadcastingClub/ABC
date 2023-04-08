89e2a27eb0ab671751cb9b632caad7e96e02cad7
c-satoru <129941982+c-satoru@users.noreply.github.com>
2023-04-08 19:59:20 +0900

    open1

--- ABC.js
+++ ABC.js
@@ -1,1 +1,19 @@
+$('.title').on('click', function() {
+    var findElm = $(this).next(".box");
+    $(findElm).slideToggle();
 
+    if($(this).hasClass('close')){
+        $(this).removeClass('close');
+    }else{
+        $(this).addClass('close');
+    }
+});
+
+$(window).on('load', function(){
+    $(".open").each(function(index, element){
+        var Title =$(element).children('.title');
+        $(Title).addClass('close');
+        var Box =$(element).children('.box');
+        $(Box).slideDown(500);
+    });
+});
