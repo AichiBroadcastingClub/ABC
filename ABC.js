25669e5ea06b0d6a90a53f3cafdb66ed29484c96
c-satoru <129941982+c-satoru@users.noreply.github.com>
2023-04-10 22:48:51 +0900

    toggle問題修正

--- ABC.js
+++ ABC.js
@@ -2,21 +2,21 @@ $('.title').on('click', function() {
     var findElm = $(this).next(".box");
     $(findElm).slideToggle();
 
-    if($(this).hasClass('close')){
-        $(this).removeClass('close');
-    }else{
-        $(this).addClass('close');
-    }
+//    if($(this).hasClass('close')){
+//        $(this).removeClass('close');
+//    }else{
+//        $(this).addClass('close');
+//    }
 });
 
-$(window).on('load', function(){
-    $(".open").each(function(index, element){
-        var Title =$(element).children('.title');
-        $(Title).addClass('close');
-        var Box =$(element).children('.box');
-        $(Box).slideDown(500);
-    });
-});
+//$(window).on('load', function(){
+//    $(".open").each(function(index, element){
+//        var Title =$(element).children('.title');
+//        $(Title).addClass('close');
+//        var Box =$(element).children('.box');
+//        $(Box).slideDown(500);
+//    });
+//});
 
 function delay(n){
     return new Promise(function(resolve){
